861768391f7c34a69924e34704c1c1da1e58346a
yurch-html <roman_yurchenko111@mail.ru>
2017-12-27 08:51:49 +0200

    c

--- src/custom-js/main.js
+++ src/custom-js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-
+//  lang
+  $('.lang-list__btn').click(function() {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+  
+  //  menu
+  $('.open-menu').click(function() {
+    $('.nav-page').toggleClass('active');
+  });
+  
+//  search
+  $('.search-btn').click(function() {
+    $('.squares-sect').toggleClass('active');
+    $('.squares-sect__search-sect').toggleClass('active');
+  });
 });
 
 
